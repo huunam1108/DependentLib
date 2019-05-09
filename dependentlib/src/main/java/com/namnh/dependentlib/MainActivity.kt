5a5a2d44c6c60d867cec10f00deeb855f48f58ba
nguyen.huu.nam <proman112111@gmail.com>
2019-05-09 10:44:46 +0700

    Release v1.4_BETA

--- dependentlib/src/main/java/com/namnh/dependentlib/MainActivity.kt
+++ dependentlib/src/main/java/com/namnh/dependentlib/MainActivity.kt
@@ -2,22 +2,14 @@ package com.namnh.dependentlib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import io.sentry.Sentry
 import io.sentry.android.AndroidSentryClientFactory
-import kotlinx.android.synthetic.main.activity_main.*
 
 open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
         Sentry.init("https://48f11c14a0c54806ad34d26a926f0345@sentry.io/1455682", AndroidSentryClientFactory(this))
     }
 }
